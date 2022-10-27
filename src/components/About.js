9fc58a8540694ea8f6a9773cf8ca968fb6b9561b
Diego R <diego-roney@outlook.com>
2022-10-26 21:11:02 -0300

    update

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import "../style/global.css";
 import "../style/about.css";
 import about from "../img/familia-plantando-juntos.jpg";
+import planta from "../img/planta.png";
 
 function About() {
   return (
@@ -14,7 +15,12 @@ function About() {
             <img src={about} alt="logo"/>
         </div>
         <div className="about-conteudo-texto">
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui oficia deserunt mollit anim id est laborum."
+       <h3>Vamos semear nosso futuro</h3>
+       <p>
+       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate;
+       </p>
+       <img src={planta} alt="logo"/>
+       
         </div>
       </div>
     </div>
