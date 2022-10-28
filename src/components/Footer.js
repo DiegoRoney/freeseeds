c2a01c6206f260785e9c929990aa7f2174dd97e2
Diego R <diego-roney@outlook.com>
2022-10-28 08:23:59 -0300

    fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,20 @@
 import "../style/global.css";
 import "../style/footer.css";
 
+function Footer() {
+  return (
+    <div className="footer text-center">
+      <img
+        src="logo-freeseeds.png"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+        alt="logo"
+      />
 
-
-function Footer(){
-    return(
-        <div className="footer text-center">
-          <img
-              src="logo-w.png"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="logo"
-            />
-          
-         <a href="https://diegoroney.github.io/portfolio/">@2022  DiegoR</a>
-
-
-        </div>
-
-    )
+      <a href="https://diegoroney.github.io/portfolio/">@2022 DiegoR</a>
+    </div>
+  );
 }
 
 export default Footer;
